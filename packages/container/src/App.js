dc754fb3d8fa83c8c8c1fabaa7929a9c5e7c49b4
Ashutosh Singh <ashutosh.chauhan837@gmail.com>
2023-12-26 10:26:08 +0530

    css conflicting issues fixed + Header added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
-export default ()=> {
-  return <div>
-    <h1>Hi There</h1>
-    <hr/>
-    <MarketingApp/>
-    </div>
-}
+// for adding prefix in production class names
+// So that classnames doesn't collide with other sub project
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
+export default () => {
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
+    </BrowserRouter>
+  );
+};
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
-
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
+// for adding prefix in production class names
+// So that classnames doesn't collide with other sub project
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={Landing}/>
-            <Route path="/pricing" component={Pricing}/>
+            <Route exact path="/" component={Landing} />
+            <Route path="/pricing" component={Pricing} />
           </Switch>
         </BrowserRouter>
       </StylesProvider>
     </div>
-  )
-}
+  );
+};
